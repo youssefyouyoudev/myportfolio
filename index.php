cf0220cef5edcbe67cae7b3e9eff8c311240880b
youssefyouyoudev <contact@youssefyouyou.com>
2026-02-10 21:56:07 +0100

    hello

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+require __DIR__.'/../vendor/autoload.php';
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
